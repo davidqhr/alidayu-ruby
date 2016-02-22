7e8c579fe05fcc769ba65997b762a3c2c8aecf99
david <david@creatingev.com>
2016-02-22 18:04:01 +0800

    update enter file

--- lib/alidayu/version.rb
+++ lib/alidayu/version.rb
@@ -1,3 +1,3 @@
 module Alidayu
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,2 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'alidayu/alidayu'
+require 'alidayu/alidayu-ruby'
